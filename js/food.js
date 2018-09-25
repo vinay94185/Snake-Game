f6c16de815f8d24175632cfef66339e8db462785
Vinay Kumar <vinay94185vinay@yahoo.com>
2018-09-25 12:24:11 +0530

    Improved food visually

--- js/food.js
+++ js/food.js
@@ -10,9 +10,14 @@ let colors = [
 class food {
 	show() {
 		this.bg.fillStyle = this.color;
+		this.bg.strokeStyle = this.color;
+		this.bg.lineWidth = 2;
 		this.bg.beginPath();
 		this.bg.arc(this.x,this.y,this.mass,0,circ);
+		this.bg.stroke();
+		this.bg.globalAlpha = 0.5;
 		this.bg.fill();
+		this.bg.globalAlpha = 1;
 		this.bg.closePath();
 	}
 	clear() {
@@ -29,7 +34,7 @@ class food {
 		this.x = x;
 		this.y = y;
 		this.bg = _bg;
-		this.mass = Math.floor((Math.random() * 4)+ 3);
+		this.mass = Math.floor((Math.random() * 3)+ 6);
 		this.color = colors[Math.floor(Math.random() * colors.length)];
 		this.show();
 	}
